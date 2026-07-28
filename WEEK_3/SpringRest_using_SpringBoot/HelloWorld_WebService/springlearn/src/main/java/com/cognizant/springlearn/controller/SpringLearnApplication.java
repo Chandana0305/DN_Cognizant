d92888cf5b2b0275982ecdc44542261775836213
Chandana0305 <salunkechandana03@gmail.com>
2026-07-29 01:27:46 +0530

    Added WEEK 3 to WEEK 6 Projects

---
+++ WEEK_3/SpringRest_using_SpringBoot/HelloWorld_WebService/springlearn/src/main/java/com/cognizant/springlearn/controller/SpringLearnApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.springlearn.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearnerApplication.class, args);
+    }
+
+}
+
---
+++ WEEK_4/Microservices/Handson_2/springlearn/src/test/java/com/cognizant/spring_learner/SpringLearnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.spring_learner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
